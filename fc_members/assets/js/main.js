9563f5ffe8706357db8814f8934d25e6e328e0b1
ten_pla <ten_pla@appare.co.uk>
2020-04-16 16:42:16 +0900

    カンストまでの必要経験値に表示切り替え

--- fc_members/assets/js/main.js
+++ fc_members/assets/js/main.js
@@ -23,11 +23,17 @@ new Vue({
     result: null,
     crest: null,
     server: null,
+    dc: null,
     fc_name: null,
     fc_tag: null,
     fc_slogan: null,
     fc_description: null,
-    fc_members: null
+    fc_members: [],
+    member: [],
+    c_result: [],
+    profs: [],
+    cj: [],
+    c_id: null,
   },
   mounted() {
     var self = this;
@@ -35,19 +41,37 @@ new Vue({
     axios
       .post('https://xivapi.com/freecompany/' + fc_id + '?data=FCM')
       .then(json => {
-        this.result = json,
-          fc = json.data.FreeCompany,
-          fc_members = json.data.FreeCompanyMembers,
-          console.dir(fc),
-          console.dir(fc_members),
+        this.result = json;
+        var fc = json.data.FreeCompany;
+        var fc_members = json.data.FreeCompanyMembers;
+          console.dir(fc);
+          console.dir(fc_members);
           fc_url = 'https://jp.finalfantasyxiv.com/lodestone/freecompany/' + fc_id + '/';
         this.crest = '<a href="' + fc_url + '" target="fc">' + '<img src="' + fc.Crest[0] + '" class="crest">' + '<img src="' + fc.Crest[1] + '" class="crest">' + '<img src="' + fc.Crest[2] + '" class="crest">' + '</a>';
-        this.server = '<span class="stitle">ワールド:</span>' + fc.Server + ' (' + fc.DC + ')';
+        this.server = fc.Server;
+        this.dc = fc.DC;
         this.fc_name = fc.Name,
-        this.fc_tag = '[' + fc.Tag + ']';
+        this.fc_tag = fc.Tag;
         this.fc_slogan = fc.Slogan;
-        this.fc_description = '<span class="stitle">メンバー数:</span><span id="fc_member_count">' + fc.ActiveMemberCount + '人</span>';
+        this.fc_description = fc.ActiveMemberCount;
         this.fc_members = fc_members;
+        // fc_membersの数だけ繰り返す。
+        fc_members.forEach((member, i) => {
+          console.log(member.ID)
+          axios.post('https://xivapi.com/character/' + member.ID + '?data=CJ')
+            // 個人データをc_jsonとして取得
+            .then(c_json => {
+              // その中のClassJobsをprofsに入れる。
+              profs = c_json.data.Character;
+              this.cj = profs.ClassJobs;
+              this.c_id = profs.ID;
+              console.log('name=' + profs.Name);
+              member['class_job'] = profs.ClassJobs;
+              console.dir(member);
+            })
+            .catch(error => console.log(error))
+        });
+        console.dir(fc_members);
       })
       .catch(error => console.log(error))
       .then(function () {
