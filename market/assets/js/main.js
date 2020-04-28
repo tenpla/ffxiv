50bfeeb09e227e0afb18808b2d4e1dff3ab15881
ten_pla <ten_pla@appare.co.uk>
2020-04-28 17:31:25 +0900

    apiにてアイテム名を取得

--- market/assets/js/main.js
+++ market/assets/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 $(function () {
   $.getJSON('https://ff14marketnoteapi.ownway.info/research/1/market_research?dc=Elemental&world=Aegis')
     .done(function (data) {
@@ -13,3 +14,61 @@ $(function () {
       console.log('FCM完了');
     });
 });
+=======
+data = [];
+str = '';
+$(function () {
+  if (localStorage.getItem('data')) {
+    console.log('データがあります');
+    data = localStorage.getItem('data');
+    console.log(JSON.parse(data));
+  } else {
+    console.log('データがありません');
+  }
+});
+$(document).on('click', '#get_data', function () {
+  $('#result').html('loading.')
+  $.ajax(
+    {
+      url: 'https://xivapi.com/Recipe?language=ja&limit=3000',
+      type: 'get',
+      datatype: 'json'
+    }
+  ).then(
+    function (data) {
+      $('#result').html('loading..');
+      $('#get_data').remove();
+      console.dir(data);
+      page_total = data.Pagination.PageTotal;
+      page = data.Pagination.Page;
+      localStorage.setItem('data', JSON.stringify(data.Results));
+    },
+    function (data) {
+      console.log(data);
+    }
+  );
+});
+
+        // if (page !== page_total) {
+        //   console.log('if=');
+        //   console.dir(data);
+        //   for (i = 2; i <= page_total; i++) {
+        //     console.log('for=');
+        //     console.dir(data);
+        //     console.log('i=' + i);
+        //     $.ajax(
+        //       {
+        //         url: 'https://xivapi.com/Recipe?language=ja&limit=3000&page=' + i,
+        //         type: 'get',
+        //         datatype: 'json'
+        //       }
+        //     )
+        //   }
+        // }
+
+        //         function (plus_data) {
+        //           $('#result').html(i + '取得');
+        //           data = data.concat(plus_data);
+        //           console.dir(data);
+        //         }
+>>>>>>> Stashed changes
