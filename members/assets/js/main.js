d692c1d41a3dc0caf1e7d0fb5e1e4c1616e66e24
ten_pla <ten_pla@appare.co.uk>
2020-07-28 10:35:42 +0900

    テスト版

--- members/assets/js/main.js
+++ members/assets/js/main.js
@@ -65,17 +65,17 @@ var app = new Vue({
   el: "#app",
   data: {
     members: [{
-        id: 29173510,
-        name: `Ten Pla`
-      },
-      {
-        id: 25658724,
-        name: `Indigo Cat`
-      },
-      {
-        id: 29367989,
-        name: `Yumeko`
-      }
+      id: 29173510,
+      name: `Ten Pla`
+    },
+    {
+      id: 25658724,
+      name: `Indigo Cat`
+    },
+    {
+      id: 29367989,
+      name: `Yumeko`
+    }
     ],
     id: 29369012,
     name: '取得中'
@@ -96,4 +96,48 @@ var app = new Vue({
       console.dir(this.members);
     }
   }
-})
+});
+
+// mounted() {
+//   var self = this;
+//   this.show = true;
+//   axios
+//     this.members = members;
+//     // membersの数だけ繰り返す。
+//     members.forEach((member, i) => {
+//       console.log(member.ID)
+//       axios.post('https://xivapi.com/character/' + member.ID + '?data=CJ')
+//         // 個人データをc_jsonとして取得
+//         .then(c_json => {
+//           // その中のClassJobsをprofsに入れる。
+//           profs = c_json.data.Character;
+//           this.cj = profs.ClassJobs;
+//           this.c_id = profs.ID;
+//           console.log('name=' + profs.Name);
+//           member['class_job'] = profs.ClassJobs;
+//           console.dir(member);
+//         })
+//         .catch(error => console.log(error))
+//     });
+//     console.dir(members);
+// }
+Vue.component('price', {
+    data: function () {
+        return {
+            items: []
+        }
+    },
+    mounted: function ()  {
+        var self = this;
+        axios
+            .get('data.json')
+                .then(function (res) {
+                    self.items = res.data;
+                });
+    },
+  template: `<table>
+      <tr v-for="item in items">
+        <td>{{ item.title }}</td><td>{{ item.price }}</td>
+      </tr>
+      </table>`
+});
